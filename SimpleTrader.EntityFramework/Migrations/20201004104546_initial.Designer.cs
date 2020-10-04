4512f5aa82042751c185ab13f30863228642bfb0
RbVs <r.b.vs@outlook.de>
2020-10-04 12:48:12 +0200

    DB context factory added

--- SimpleTrader.EntityFramework/Migrations/20201004104546_initial.Designer.cs
+++ SimpleTrader.EntityFramework/Migrations/20201004104546_initial.Designer.cs
@@ -10,7 +10,7 @@ using SimpleTrader.EntityFramework;
 namespace SimpleTrader.EntityFramework.Migrations
 {
     [DbContext(typeof(SimpleTraderDbContext))]
-    [Migration("20201004103304_initial")]
+    [Migration("20201004104546_initial")]
     partial class initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -51,6 +51,9 @@ namespace SimpleTrader.EntityFramework.Migrations
                     b.Property<int?>("AccountId")
                         .HasColumnType("int");
 
+                    b.Property<DateTime>("DateProcessed")
+                        .HasColumnType("datetime2");
+
                     b.Property<bool>("IsPurchase")
                         .HasColumnType("bit");
 
@@ -71,10 +74,16 @@ namespace SimpleTrader.EntityFramework.Migrations
                         .HasColumnType("int")
                         .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
+                    b.Property<DateTime>("DateJoined")
+                        .HasColumnType("datetime2");
+
                     b.Property<string>("Email")
                         .HasColumnType("nvarchar(max)");
 
-                    b.Property<string>("UserName")
+                    b.Property<string>("Password")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Username")
                         .HasColumnType("nvarchar(max)");
 
                     b.HasKey("Id");
---
+++ SimpleTrader.EntityFramework/SimpleTraderDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace SimpleTrader.EntityFramework
+{
+    public class SimpleTraderDbContextFactory : IDesignTimeDbContextFactory<SimpleTraderDbContext>
+    {
+        public SimpleTraderDbContext CreateDbContext(string[] args)
+        {
+            var options = new DbContextOptionsBuilder<SimpleTraderDbContext>();
+            options.UseSqlServer(
+                @"Server=(localdb)\MSSQLLocalDB; Database=SimpleTraderDB;Trusted_Connection=True;");
+            return new SimpleTraderDbContext(options.Options);
+        }
+    }
+}
